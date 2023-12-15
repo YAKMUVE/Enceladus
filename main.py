286bc5c0b5fcb5eee863f09a6f28bc41dca37c8e
P1 <pop@mail.ru>
2023-12-15 17:19:38 +0300

    Комментирование функций

--- main.py
+++ main.py
@@ -27,6 +27,13 @@ immovable = pygame.sprite.Group()
 
 class Object(pygame.sprite.Sprite):
     def __init__(self, width: int, height: int, path: str, x: int, y: int):
+        """
+        :width: длина объекта
+        :height: высота объекта
+        :path: путь до спрайта объекта
+        :x: координата объекта по оски x
+        :y: координата объекта по оски y
+        """
         pygame.sprite.Sprite.__init__(self)
         self.width = width
         self.height = height
@@ -42,9 +49,14 @@ class Object(pygame.sprite.Sprite):
 
 class Player(Object):
     def __init__(self, x: int, y: int):
-        super().__init__(64, 64, 'Sprites\\char_sprite_1.png', x, y)
+        """
+        :x: координата объекта по оски x
+        :y: координата объекта по оски y
+        """
+        super().__init__(64, 64, 'Sprites/general/char_sprite_1.png', x, y)
         self.health = 3
 
+        # трекер движения игрока
         self.upward = False
         self.downward = False
         self.leftward = False
@@ -56,26 +68,36 @@ class Player(Object):
         self.border_control()
 
     def definition(self, key: int):
+        """
+        definition отслеживает передвижения игрока
+
+        :key: ключ (id) нажатой кнопки
+        """
         if key == K_UP and not self.tracking(): self.upward = True
         if key == K_DOWN and not self.tracking(): self.downward = True
         if key == K_LEFT and not self.tracking(): self.leftward = True
         if key == K_RIGHT and not self.tracking(): self.rightward = True
 
     def tracking(self) -> bool:
+        """
+        :return: направляется ли уже куда-то игрок
+        """
         valid = self.upward or self.downward or self.leftward or self.rightward
         return valid
 
     def movement(self):
-        if self.upward:
-            self.rect.y -= MOVE_SPEED
-        if self.downward:
-            self.rect.y += MOVE_SPEED
-        if self.leftward:
-            self.rect.x -= MOVE_SPEED
-        if self.rightward:
-            self.rect.x += MOVE_SPEED
+        """
+        movement передвигает игрока
+        """
+        if self.upward: self.rect.y -= MOVE_SPEED
+        if self.downward: self.rect.y += MOVE_SPEED
+        if self.leftward: self.rect.x -= MOVE_SPEED
+        if self.rightward: self.rect.x += MOVE_SPEED
 
     def conflict(self):
+        """
+        confict останавливает игрока, в случае столкновения с другим объектом
+        """
         hit = pygame.sprite.spritecollide(player, immovable, False)
 
         for collision in hit:
@@ -85,9 +107,13 @@ class Player(Object):
             if self.rightward: self.rightward = False
 
     def border_control(self):
+        """
+        border_control останавливает игрока, в случае выхода за границу
+        """
         if self.rect.right > WIN_WIDTH:
             self.rect.right = WIN_WIDTH
             self.rightward = False
+
         if self.rect.left < 0:
             self.rect.left = 0
             self.leftward = False
@@ -95,21 +121,22 @@ class Player(Object):
         if self.rect.bottom > WIN_HEIGHT:
             self.rect.bottom = WIN_HEIGHT
             self.downward = False
+
         if self.rect.top < 0:
             self.rect.top = 0
             self.upward = False
 
 
-class Camera(object):
-    def __init__(self, camera_func, width, height):
-        self.camera_func = camera_func
-        self.state = pygame.Rect(0, 0, width, height)
-
-    def apply(self, target):
-        return target.rect.move(self.state.topleft)
-
-    def update(self, target):
-        self.state = self.camera_func(self.state, target.rect)
+# class Camera(object):
+#     def __init__(self, camera_func, width, height):
+#         self.camera_func = camera_func
+#         self.state = pygame.Rect(0, 0, width, height)
+#
+#     def apply(self, target):
+#         return target.rect.move(self.state.topleft)
+#
+#     def update(self, target):
+#         self.state = self.camera_func(self.state, target.rect)
 
 
 def render():
