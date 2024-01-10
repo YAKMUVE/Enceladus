e49fc53af025ba990238e47b336e0f54a0eec964
YAKMUVE <rinatibragimov2018@gmail.com>
2024-01-10 10:15:34 +0300

    Готовая сборка

--- main.py
+++ main.py
@@ -37,6 +37,34 @@ entities = pygame.sprite.Group()
 classes = pygame.sprite.Group()
 
 
+# db
+# def database_maker():
+#     con = sq.connect('progress.db')
+#     cur = con.cursor()
+#     cur.execute("""
+#     CREATE TABLE IF NOT EXISTS 'achievements'(
+#         'level'   TEXT,
+#         'sum' INTEGER,
+#         'weapon'  TEXT,
+#         'extra'   TEXT
+#     );
+#     """)
+#     con.commit()
+#     r = cur.execute("""SELECT * FROM 'achievements'""").fetchall()
+#     if not r:
+#         cur.execute("""INSERT INTO 'achievements'(level) VALUES('0')""")
+#         con.commit()
+#     con.close()
+#
+#
+# def database_changer(column, value):
+#     con = sq.connect('progress.db')
+#     cur = con.cursor()
+#     cur.execute(f"UPDATE 'achievements' SET {column}=?", (value, ))
+#     con.commit()
+#     con.close()
+
+
 class Object(pygame.sprite.Sprite):
     def __init__(self, width: int, height: int, path: str, x: int, y: int):
         """
